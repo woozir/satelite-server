100bf0d9c198bb2b4a1df87be3b04f8a4bd5a9ec
maz-dev <michael.mazurczak@gmail.com>
2020-11-14 15:52:07 +0100

    boilerplate modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PresenceModule } from './presence/presence.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PresenceModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
